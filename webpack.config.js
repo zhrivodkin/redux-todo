e34c27d57ecb8f96b541651c3a3f3ee0d5480dfd
Max Tolkachev <zhrivodkin@gmail.com>
2017-07-07 17:24:38 +0300

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+var path = require('path')
+var webpack = require('webpack')
+
+module.exports = {
+  entry: [
+    'webpack-hot-middleware/client',
+    './src/index'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/static/'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+  ]
+}
